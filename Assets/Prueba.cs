ffa4d77b7344624374151d75f3040d71b87088c2
Maiaguiter <49686906@est.ort.edu.ar>
2022-04-13 15:32:06 -0300

    finished

---
+++ Assets/Prueba.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    public int dia;
+    public string codigo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int G = dia * 300;
+        int PP = dia * 400;
+        int PG = dia * 700;
+        int precioG = G / 100 * 80;
+        int precioPP = PP / 100 * 80;
+        int precioPG = PG / 100 * 80;
+
+        if (dia < 3)
+        {
+            Debug.Log("La cantidad de dias debera ser mayor a tres");
+        }
+        if (codigo != "G" && codigo != "PP" && codigo != "PG")
+        {
+            Debug.Log("El codigo ingresado no es valido");
+        }
+        else if (codigo == "G")
+        {
+            Debug.Log("Para ese periodo se necesitan " + G + " gramos y saldra " + precioG + "pesos");
+        }
+        else if (codigo == "PP")
+        {
+            Debug.Log("Para ese periodo se necesitan " + PP + " gramos y saldra " + precioPP + "pesos");
+        }
+        else if (codigo == "PG")
+        {
+            Debug.Log("Para ese periodo se necesitan " + PG + " y saldra " + precioPG + "pesos");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
